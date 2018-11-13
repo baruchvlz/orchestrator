7682b1f48c84306011030e52daa8fd576ae5317a
Baruch Velez <baruchvelez@gmail.com>
2018-11-13 19:30:27 +0100

    chore: add watch scripts for testing

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "moduleNameMapper": {
-    "@orchestrator/core": "<rootDir>/libs/core/src/public_api.ts",
-    "@orchestrator/layout": "<rootDir>/libs/layout/src/public_api.ts",
-    "@orchestrator/core/testing": "<rootDir>/libs/core/src/lib/testing/src/public_api.ts"
+  moduleNameMapper: {
+    '@orchestrator/core': '<rootDir>/libs/core/src/public_api.ts',
+    '@orchestrator/layout': '<rootDir>/libs/layout/src/public_api.ts',
+    '@orchestrator/core/testing': '<rootDir>/libs/core/src/lib/testing/src/public_api.ts'
   },
-  "modulePathIgnorePatterns": [
-    "<rootDir>/dist"
-  ]
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist',
+  ],
 };
